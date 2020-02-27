3c9335d45d9c2e9264894ec4d74b938e359dcf62
Werdon4 <filipgajewski4@gmail.com>
2020-02-27 17:39:13 +0100

    Add function drawing simple junction
    In main Functiom draw_crossing.
    It draw junction from dots.
    Arguments are number of road
    lanes and slots for cars.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <ncurses.h>
+#include <string.h>
+
+void draw_crossing(int lanes, int slots)
+{
+    int dimensions = 0;
+    dimensions = getmaxy(stdscr);
+    
+    //horizontal lines
+    for(int i = 0; i<lanes; ++i){
+        for(int j = 0; j<2*slots+lanes; j++){
+            mvaddch(dimensions/2+i, j +dimensions/2,'.');
+        };
+    }
+    //vertical lines
+    for(int i = 0; i<lanes; ++i){
+        for(int j = 0; j<2*slots+lanes; j++){
+            mvaddch(j+dimensions/2-slots, i+slots +dimensions/2, '.');
+        };
+    }
+
+}
 
 int main()
 {
-    initscr();
-    printw("Hello!");
+    initscr();//initializes the screen and sets up memory and clears the screen
+    draw_crossing(4,10);
     getch();
-    endwin();
+
+    endwin();//deallocates memory and ends ncurses
 
     return 0;
 }
